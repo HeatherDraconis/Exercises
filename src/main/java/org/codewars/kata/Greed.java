6bdbb7ed9c97a85675d76481d2c6a729e86814dd
HeatherDraconis <heather.nuttall@hotmail.com>
2024-07-24 16:34:43 +0100

    Tidy up.

--- src/main/java/org/codewars/kata/Greed.java
+++ src/main/java/org/codewars/kata/Greed.java
@@ -3,60 +3,32 @@ package org.codewars.kata;
 public class Greed {
     public static int greedy(int[] dice){
         int points = 0;
-        int one = 0;
-        int two = 0;
-        int three = 0;
-        int four = 0;
-        int five = 0;
-        int six = 0;
-        for (int i = 0; i < dice.length; i++) {
-            if (dice[i] == 1) {
-                one += 1;
-            }
-            if (dice[i] == 2) {
-                two += 1;
-            }
-            if (dice[i] == 3) {
-                three += 1;
-            }
-            if (dice[i] == 4) {
-                four += 1;
-            }
-            if (dice[i] == 5) {
-                five += 1;
-            }
-            if (dice[i] == 6) {
-                six += 1;
-            }
+        int[] num = new int[6];
+        for (int roll : dice) {
+            num[roll - 1] += 1;
         }
-        if (one >= 3) {
+        if (num[0] >= 3) {
             points += 1000;
-            one -= 3;
+            num[0] -= 3;
         }
-        if (six >= 3) {
+        if (num[5] >= 3) {
             points += 600;
         }
-        if (five >= 3) {
+        if (num[4] >= 3) {
             points += 500;
-            five -= 3;
+            num[4] -= 3;
         }
-        if (four >= 3) {
+        if (num[3] >= 3) {
             points += 400;
         }
-        if (three >= 3) {
+        if (num[2] >= 3) {
             points += 300;
         }
-        if (two >= 3) {
+        if (num[1] >= 3) {
             points += 200;
         }
-        while (one > 0) {
-            points += 100;
-            one -= 1;
-        }
-        while (five > 0) {
-            points += 50;
-            five -= 1;
-        }
+        points += num[0] * 100;
+        points += num[4] * 50;
         return points;
     }
 }
--- src/main/java/org/codewars/kata/NoRepeat.java
+++ src/main/java/org/codewars/kata/NoRepeat.java
@@ -4,7 +4,10 @@ public class NoRepeat {
     public static String firstNonRepeatingLetter(String s){
         String lowerS = s.toLowerCase();
         for (int i = 0; i < s.length(); i++) {
-            if ((lowerS.substring(0, i).indexOf(lowerS.charAt(i)) == -1) && (lowerS.substring(i + 1).indexOf(lowerS.charAt(i)) == -1)) {
+            char ch = lowerS.charAt(i);
+            boolean chNotPresentBefore = lowerS.substring(0, i).indexOf(ch) == -1;
+            boolean chNotPresentAfter = lowerS.substring(i + 1).indexOf(ch) == -1;
+            if (chNotPresentBefore && chNotPresentAfter) {
                 return s.charAt(i) + "";
             }
         }
