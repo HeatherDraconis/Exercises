959a3c0270e8f45e236aed9283b8c1f9a9461086
HeatherDraconis <heather.nuttall@hotmail.com>
2025-06-10 16:43:59 +0100

    Added attempt for "Pyramid Slide Down" Kata.

---
+++ src/main/java/org/codewars/kata/LongestSlideDown.java
@@ -0,0 +1,12 @@
+package org.codewars.kata;
+
+public class LongestSlideDown {
+    public static int longestSlideDown(int[][] pyramid) {
+        for (int i = pyramid.length - 2; i >= 0; i--) {
+            for (int j = 0; j < pyramid[i].length; j++) {
+                pyramid[i][j] += Math.max(pyramid[i + 1][j], pyramid[i + 1][j + 1]);
+            }
+        }
+        return pyramid[0][0];
+    }
+}
